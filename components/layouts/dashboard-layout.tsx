e6ed26867a7b3a54fc048548c7b248aff283f079
Goodness Adewuyi <goodnessadewuyi8@gmail.com>
2025-04-07 15:06:50 +0000

    Made minor changes

--- components/layouts/dashboard-layout.tsx
+++ components/layouts/dashboard-layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import DashboardSidebar from "./dashboard-sidebar";
+import DashboardSidebar from "../dashboard/dashboard-sidebar";
 import BackgroundElements from "../ui/background-elements";
 
 interface DashboardLayoutProps {
